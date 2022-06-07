d850cc231b1e6c7291c46665969aa298094205a5
kirillknz <104511811+kirillknz@users.noreply.github.com>
2022-06-07 14:18:24 +0300

    add test button basket

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_btn_add_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    #time.sleep(30)
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
