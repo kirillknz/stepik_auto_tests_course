1bdbda372a99e5f7c418192530b6cdccd59adbb2
kirillknz <104511811+kirillknz@users.noreply.github.com>
2022-06-07 14:18:50 +0300

    Add test button basket

--- test_items.py
+++ test_items.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
+
 def test_btn_add_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
